539e02e6f98a48500c2633757427120013964d3e
Divine Gb <ado.gbagi@gmail.com>
2023-11-25 11:43:32 -0600

    adding project

---
+++ personClass/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace personClass{
+    class Person{
+    int nextID = 100;
+    int population = 0;
+
+    string name;
+    int id;
+
+    public Person(string _name){
+        name = _name;
+        id = nextID += 10;
+        population += 1;
+    }
+
+    public string Name{
+        get{ return name;}
+        set{ name = value;}
+    }
+
+    public int ID{
+        get{ return id;}
+    }
+
+    public int Population{
+        get{ return population;}
+    }
+}
+
+    class Student : Person{
+        int bioMajors = 0;
+        public string name;
+        public string major;
+
+        public Student(string _name, string _major) : base( _name)
+        {
+            string major = _major;
+            if (_major == "Biology"){
+                bioMajors += 1;
+            }
+            name = _name;
+        }
+
+        public string Major{ get {return major;}}
+        public int BioMajors{ get { return bioMajors;}}
+    }
+    class Program{
+        static void Main(string[] args)
+    {
+        Person p1 = new Person("Sally");
+        Console.WriteLine(p1.Name);
+        Student s1 = new Student("Sam","Biology");
+        Console.WriteLine(s1.Major + s1.ID);
+        Console.ReadKey();
+    }
+        
+    }
+    
+}
