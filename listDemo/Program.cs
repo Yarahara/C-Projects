1c28468fd3a0ec2d5545ff905c40da9a83ddbdd8
Divine Gb <ado.gbagi@gmail.com>
2023-12-01 18:58:21 -0600

    adding project

---
+++ listDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace listDemo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>
+        {
+            12, 10, 32, 3, 66, 17, 42, 99, 20
+        };
+        foreach (int num in numList)
+        {
+            Console.WriteLine(num );
+        }
+        }
+        
+    }
+}
---
+++ listDemo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace listDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>
+            {
+                12, 10, 32, 3, 66, 17, 42, 99, 20
+            };
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine($"{num} {num*num}");
+            }
+        }
+    }
+}
